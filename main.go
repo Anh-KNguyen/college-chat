3809ea974da2d8eb7ab5eaebbecbe76b31981707
Anh Nguyen <anhknguyen97@yahoo.com>
2020-06-15 21:58:32 -0700

    Add comments to rest api

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"net/http"
 )
 
+// handle all requests to root URL
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+// match the URL path hit with a defined function
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+// kick off API
 func main() {
 	handleRequests()
 }
