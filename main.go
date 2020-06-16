208700ff2235ea6e53077120c9194e013bff778b
Anh Nguyen <anhknguyen97@yahoo.com>
2020-06-16 00:43:09 -0700

    Return single article based on Id URL value

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", returnAllArticles) // add articles route and map to returnAllArticles function
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
@@ -29,14 +30,15 @@ func main() {
 	fmt.Println("REST API v2.0 - Mux Routers")
 	// populate Articles with dummy data
 	Articles = []Article{
-		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		{Title: "Hello2", Desc: "Article Description", Content: "Article Content"},
+		{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Id: "2", Title: "Hello2", Desc: "Article Description", Content: "Article Content"},
 	}
 	handleRequests()
 }
 
 // Article struct for title, description, and content
 type Article struct {
+	Id      string `json:"Id"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -49,4 +51,18 @@ var Articles []Article
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllArticles")
 	json.NewEncoder(w).Encode(Articles) // encodes articles array into JSON string and write as part of response
+}
+
+// return single article based on {id} value from URL
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	// loop through articles and return matched article as JSON
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+
 }
