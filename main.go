fb851e88e084451ee1ba38a4673b671a74a4022c
Anh Nguyen <anhknguyen97@yahoo.com>
2020-06-15 22:25:47 -0700

    Create REST API and add route to handleRequests func

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,11 +16,13 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 // match the URL path hit with a defined function
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles) // add articles route and map to returnAllArticles function
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 // kick off API
 func main() {
+	// populate Articles with dummy data
 	Articles = []Article{
 		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
 		Article{Title: "Hello2", Desc: "Article Description", Content: "Article Content"},
@@ -36,3 +39,9 @@ type Article struct {
 
 // global Articles array to populate in main function to simulate a database
 var Articles []Article
+
+// create REST endpoint when hit with a HTTP GET request, will return all articles
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles) // encodes articles array into JSON string and write as part of response
+}
